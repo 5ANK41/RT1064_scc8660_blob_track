e97095a504410b87cb8314baf5d6714a20ac58fc
5ANK41 <5ANK.4ON@gmail.com>
2020-01-23 22:49:33 +0800

    merge

--- RT1064_scc8660_blob_track/USER/src/main.c
+++ RT1064_scc8660_blob_track/USER/src/main.c
@@ -13,9 +13,9 @@ int main(void)
       find_blobs();
       if(pix_num != 0)
         draw_rtg(beacon_x,beacon_y,pix_num,1);
-      ips200_displayimage8660_zoom(img_show[0],160,120,160,120);
+      ips200_displayimage8660_zoom(img_show[0],160,120,320,240);
     }
-    BlobShow();
+    //BlobShow();
     //ThresholdCtl();
     //ThresholdShow();
   }
